41afe2d4ee1ea9a73e04aecbb1493833a8a6ab6d
karvind08 <karvind08@gmail.com>
2025-02-26 19:47:56 +0530

    26 February 2025

--- App1/main.py
+++ App1/main.py
@@ -1,8 +1,23 @@
 import streamlit as st
 st.title("My First Streamlit app")
-st.text("This is text\n[and more text](that's not a Markdown link).")
-st.text("This is another text",help='This is streamlit text')
+st.text("This is text\n[and more text](that's not a Markdown link).\n")
+st.text("\nThis is another text",help='This is streamlit text')
 
 code = '''def hello():
-    print("Hello, Streamlit!")'''
-st.code(code, language="python")
+    print("Hello, Streamlit!")
+    print('I am Arvind')'''
+st.code(code, language="python",line_numbers=True,wrap_lines=True, height=80)
+code1 = '''Is it a crown or boat?
+                        ii
+                      iiiiii
+WWw                 .iiiiiiii.                ...:
+ WWWWWWw          .iiiiiiiiiiii.         ........
+  WWWWWWWWWWw    iiiiiiiiiiiiiiii    ...........
+   WWWWWWWWWWWWWWwiiiiiiiiiiiiiiiii............
+    WWWWWWWWWWWWWWWWWWwiiiiiiiiiiiiii.........
+     WWWWWWWWWWWWWWWWWWWWWWwiiiiiiiiii.......
+      WWWWWWWWWWWWWWWWWWWWWWWWWWwiiiiiii....
+       WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWwiiii.
+          -MMMWWWWWWWWWWWWWWWWWWWWWWMMM-
+'''
+st.code(code1, language=None)
