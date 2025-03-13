4ae91f90aba8db8d2b9980cb054e6eb7e8026918
karvind08 <karvind08@gmail.com>
2025-03-13 16:05:27 +0530

    13 March 2025

--- App2/views/about_me.py
+++ App2/views/about_me.py
@@ -1,2 +1,12 @@
 import streamlit as st
 st.title("About us")
+
+col1,col2 = st.columns(2,gap="small",vertical_alignment="center")
+
+with col1:
+    st.image("./assets/logo.png", width=150)
+
+
+with col2:
+    st.title("Arvind", anchor=False)
+    st.write("Founder, Actolaze Technology LLP, Kharwal Academy - The School of Programming")
