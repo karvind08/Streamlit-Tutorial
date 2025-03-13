87cf49dcd5536575fbc02a598100d997437db383
karvind08 <karvind08@gmail.com>
2025-03-13 15:17:51 +0530

    13 March 2025

--- App2/main.py
+++ App2/main.py
@@ -1,1 +1,23 @@
 import streamlit as st
+
+about_page = st.Page(
+    page = "views/about_me.py",
+    title = "About Me",
+    icon = ":material/account_circle:",
+    default = True,
+)
+
+project_1_page = st.Page(
+    page = "views/sales_dashboard.py",
+    title = "Sales Dashboard",
+    icon = ":material/bar_chart:",
+)
+
+project_2_page = st.Page(
+    page = "views/chatbot.py",
+    title = "Chat Bot",
+    icon = ":material/smart_toy:",
+)
+
+pg = st.navigation(pages=[about_page,project_1_page,project_2_page])
+pg.run()
--- App2/views/sales_dashboard.py
+++ App2/views/sales_dashboard.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.title("Chat Bot")
+st.title("Dashboard")
