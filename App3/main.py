8a58a6912c3fa171299cb7d98c8656341c7a8e11
karvind08 <karvind08@gmail.com>
2025-03-13 21:07:48 +0530

    13 March 2025

--- App3/main.py
+++ App3/main.py
@@ -17,6 +17,16 @@ with st.sidebar:
         default_index= 0,       
     )
 
+
+selected = option_menu(
+        menu_title=None,
+        options = ["Home","About","Contact"],
+        menu_icon=":material/account-circle:",
+        icons  = ['house','book','person-rolodex'],
+        default_index= 0,    
+        orientation= "horizontal",
+    )
+
 if selected == "Home":
     st.title(f"You have selected {selected}")
 
