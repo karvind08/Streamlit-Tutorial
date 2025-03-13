0143d01a3401a8d329b85bf7732ad93d8027a998
karvind08 <karvind08@gmail.com>
2025-03-13 21:02:59 +0530

    13 March 2025

--- App3/main.py
+++ App3/main.py
@@ -1,12 +1,22 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+# with st.sidebar:
+#     selected = option_menu(
+#         menu_title="Main Menu",
+#         menu_icon=":material/account-circle:",
+#         options = ["Home","About","Contact"]
+#     )
+
 with st.sidebar:
     selected = option_menu(
-        menu_title="Main Menu",
+        menu_title=None,
+        options = ["Home","About","Contact"],
         menu_icon=":material/account-circle:",
-        options = ["Home","About","Contact"]
+        icons  = ['house','book','person-rolodex'],
+        default_index= 0,       
     )
+
 if selected == "Home":
     st.title(f"You have selected {selected}")
 
