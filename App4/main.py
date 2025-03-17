5f3d36bf33353cbd20e93265faf7acd8e673bf51
karvind08 <karvind08@gmail.com>
2025-03-17 21:21:21 +0530

    17 March 2025

--- App4/main.py
+++ App4/main.py
@@ -1,2 +1,15 @@
 import streamlit as st
-st.sidebar.selectbox("Select a Number:",[1,2,3,4,5])
+import pandas as pd
+from matplotlib import pyplot as plt
+plt.style.use("ggplot")
+data = {
+    'num': [x for x in range(1,11)],
+    'square':[x for x in range(1,11)],
+    'twice': [x for x in range(1,11)],
+    'thrice': [x for x in range(1,11)]
+}
+df = pd.DataFrame(data= data)
+#st.sidebar.selectbox("Select a Number:",[1,2,3,4,5])
+col = st.sidebar.selectbox('Select a Column',df.columns)
+plt.plot(df['num'],df[col])
+st.pyplot(fig=None)
