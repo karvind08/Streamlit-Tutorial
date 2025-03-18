a22918d9f6daa7fb06a4c1a07720b2000af1de61
karvind08 <karvind08@gmail.com>
2025-03-18 16:03:50 +0530

    18 March 2025

--- App4/plot.py
+++ App4/plot.py
@@ -29,7 +29,22 @@ st.bar_chart(data)
 # Altair
 st.subheader('Altair ')
 chart = alt.Chart(data).mark_circle().encode(
-    x = 'A',
-    y = 'B'
+    x = 'A', y = 'B',tooltip= ['A','B']
 )
-st.altair_chart(chart)
+st.graphviz_chart("""
+diagraph {
+    watch -> like
+    like ->share
+    share -> subscribe
+    share -> watch                  
+}
+""")
+st.altair_chart(chart,use_container_width=True)
+
+city = pd.DataFrame(
+{
+    'mycity': ['Delhi','Chennai','Kolkata'],
+    'lat': [28.679079,13.067439,22.572645],
+    'lon': [77.069710,80.237617,88.363892],
+})
+st.map(city)
