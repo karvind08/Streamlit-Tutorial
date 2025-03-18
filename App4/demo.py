de91f3d6fc48f29db87101351483fbdc47fca5a6
karvind08 <karvind08@gmail.com>
2025-03-18 13:43:18 +0530

    18 March 2025

--- App4/demo.py
+++ App4/demo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+# Data 
 a = [1,2,3,4,5,6,7,8]
 n = np.array(a)
 nd = n.reshape((2,4))
@@ -10,4 +10,9 @@ dict = {
     'Age': ['Three','Four'],
     'Gender':['Female','Male'],
 }
+# Table in streamlit
+
 st.table(dict)
+st.markdown(''' *** ''')
+# Json File
+st.json(dict)
