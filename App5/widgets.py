9ade7afd5833bc8a677c249c3c989e4a98806df4
karvind08 <karvind08@gmail.com>
2025-03-18 16:29:15 +0530

    18 March 2025

--- App5/widgets.py
+++ App5/widgets.py
@@ -6,3 +6,14 @@ st.subheader("Buttons")
 st.button("I am Button")
 if st.button("Subscribe"):
     st.write("Please subscribe")
+
+st.subheader("Text Input")
+st.text_input("First Name")
+
+name = st.text_input("Name")
+st.write(name)
+
+st.subheader("Text Area")
+address = st.text_area("Enter the address")
+st.write(address)
+
