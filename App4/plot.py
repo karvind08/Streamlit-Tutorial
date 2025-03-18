a07da0bd8feff74fdd8373b6a2cff055b86368a7
karvind08 <karvind08@gmail.com>
2025-03-18 14:18:10 +0530

    18 March 2025

--- App4/plot.py
+++ App4/plot.py
@@ -1,16 +1,29 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
+
 
 data = pd.DataFrame(
     np.random.randn(100,3),
     columns= ['A','B','C']
 )
 
-# Line chart 
+# Line Chart 
 st.subheader('Line Chart')
 st.line_chart(data)
 
-# Area chart 
+# Area Chart 
 st.subheader('Area Chart')
 st.area_chart(data)
+
+# Bar Chart 
+st.subheader('Bar Chart')
+st.bar_chart(data)
+
+# Matplotlib
+plt.scatter(data['A'],data['B'])
+plt.title("Scatter Plot using Matplotlib")
+st.pyplot()
+
+
