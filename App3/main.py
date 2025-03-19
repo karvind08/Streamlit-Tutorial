4797df503ba1a5c85a4bbfcf9abbbde1fda4ccb8
karvind08 <karvind08@gmail.com>
2025-03-19 19:24:26 +0530

    19 March 2025

--- App3/main.py
+++ App3/main.py
@@ -8,14 +8,14 @@ from streamlit_option_menu import option_menu
 #         options = ["Home","About","Contact"]
 #     )
 
-with st.sidebar:
-    selected = option_menu(
-        menu_title=None,
-        options = ["Home","About","Contact"],
-        menu_icon=":material/account-circle:",
-        icons  = ['house','book','person-rolodex'],
-        default_index= 0,       
-    )
+# with st.sidebar:
+#     selected = option_menu(
+#         menu_title=None,
+#         options = ["Home","About","Contact"],
+#         menu_icon=":material/account-circle:",
+#         icons  = ['house','book','person-rolodex'],
+#         default_index= 1,       
+#     )
 
 
 selected = option_menu(
@@ -26,12 +26,9 @@ selected = option_menu(
         default_index= 0,    
         orientation= "horizontal",
     )
-
 if selected == "Home":
     st.title(f"You have selected {selected}")
-
 if selected == "About":
     st.title(f"You have selected {selected}")
-
 if selected == "Contact":
     st.title(f"You have selected {selected}")
