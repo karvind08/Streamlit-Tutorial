77a897c5c2b7ff9e356058dd108f75ccad082a7c
karvind08 <karvind08@gmail.com>
2025-03-19 18:40:49 +0530

    19 March 2025

--- App6/sessions.py
+++ App6/sessions.py
@@ -2,15 +2,17 @@ import streamlit as st
 st.title("Session States")
 st.header("Counter")
 
+def add_one():
+    st.session_state.counter +=1
+
 if 'counter' not in st.session_state:
     st.session_state['counter'] = 0
+
 count = st.session_state.counter 
+ 
+st.button('Add One',on_click=add_one)
 
-{
-    'counter':0
-}
-
-if increment := st.button("Add 1"):
-    count = count + 1
-    st.session_state.counter = count
+# if increment := st.button("Add 1"):
+#     count = count + 1
+#     st.session_state.counter = count
 st.write(count)
